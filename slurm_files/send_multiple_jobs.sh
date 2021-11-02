7cfc0b85ed2b91d8fcfc3bad15f7b66193038a42
rupof <robertoflorezablan@estudante.ufscar.br>
2021-11-02 20:14:08 -0300

    correct full jobs implementation

--- slurm_files/send_multiple_jobs.sh
+++ slurm_files/send_multiple_jobs.sh
@@ -4,16 +4,13 @@
 angle1=$1
 angle2=$2
 N=$3
-useb0=$4
-kd=$5
-exc_radius=1 
-Description=$7
-num_runs=$8
+Description=$4
+num_runs=$5
 
 
 for (( set_number=1; set_number<=$num_runs; set_number++ ))
 do
    echo "Sending job angle1=$angle1 angle2=$angle2 N=$N  Description=$Description num_runs=$num_runs "	
-   sbatch job_wave.sh $angle1 $angle2 $N $useb0 $kd $exc_radius  $Description $num_runs      
+   sbatch job_wave.sh $angle1 $angle2 $N $Description      
 done
 
